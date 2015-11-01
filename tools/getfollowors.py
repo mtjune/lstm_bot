7e9583a78bd93d1cd9d527814d0bdd63576c8898
Yukich <june.ymd@gmail.com>
2015-11-01 15:16:38 +0900

    fix

--- tools/getfollowors.py
+++ tools/getfollowors.py
@@ -11,10 +11,10 @@ keys_lstmbot = None
 with open('keys_lstmbot.yml', 'r') as f:
     keys_lstmbot = yaml.load(f)
 
-with open('keys_mtjuney.yml', 'r') as f:
-    keys_mtjuney = yaml.load(f)
+# with open('keys_mtjuney.yml', 'r') as f:
+#     keys_mtjuney = yaml.load(f)
 
-api = OAuth1Session(keys['CONSUMER_KEY'], keys['CONSUMER_SECRET'], keys['ACCESS_TOKEN'], keys['ACCESS_SECRET'])
+api = OAuth1Session(keys_lstmbot['CONSUMER_KEY'], keys_lstmbot['CONSUMER_SECRET'], keys_lstmbot['ACCESS_TOKEN'], keys_lstmbot['ACCESS_SECRET'])
 url = "https://api.twitter.com/1.1/friends/ids.json"
 
 screen_list = ['mtjuney']
